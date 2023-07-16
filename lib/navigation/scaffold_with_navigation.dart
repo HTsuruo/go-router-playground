63955753c2dd0850e0a607274de011630e04a628
htsuruo <tsuru.dev@gmail.com>
2023-07-17 08:58:07 +0900

    Keep page state to use StatefulShellRoute

--- lib/navigation/scaffold_with_navigation.dart
+++ lib/navigation/scaffold_with_navigation.dart
@@ -3,23 +3,25 @@ import 'package:go_router/go_router.dart';
 
 import 'navigation_item.dart';
 
-class ScaffoldWithNavBar extends StatelessWidget {
-  const ScaffoldWithNavBar({
+class ScaffoldWithNavigation extends StatelessWidget {
+  const ScaffoldWithNavigation({
     super.key,
-    required this.child,
+    required this.navigationShell,
   });
 
-  final Widget child;
+  final StatefulNavigationShell navigationShell;
 
   @override
   Widget build(BuildContext context) {
-    final location = GoRouterState.of(context).location;
     return Scaffold(
-      body: child,
+      body: navigationShell,
       bottomNavigationBar: NavigationBar(
-        selectedIndex: NavigationItem.getFromLocation(location).index,
+        selectedIndex: navigationShell.currentIndex,
         onDestinationSelected: (index) {
-          context.go(NavigationItem.values[index].location);
+          navigationShell.goBranch(
+            index,
+            initialLocation: index == navigationShell.currentIndex,
+          );
         },
         destinations: [
           for (final item in NavigationItem.values)
--- lib/router.dart
+++ lib/router.dart
@@ -5,42 +5,52 @@ import 'package:go_router_playground/features/third_page.dart';
 
 import 'features/first/detail_page.dart';
 import 'features/first/first_page.dart';
-import 'navigation/scaffold_with_nav_bar.dart';
+import 'navigation/scaffold_with_navigation.dart';
 
 final router = GoRouter(
   debugLogDiagnostics: kDebugMode,
   initialLocation: '/',
   routes: [
-    ShellRoute(
-      builder: (context, state, child) => ScaffoldWithNavBar(child: child),
-      //
-      // pageBuilder: (context, state, child) => _FadeTransitionRoute(
-      //   child: NavigationBarScaffold(state: state, child: child),
-      // ),
-      routes: [
-        GoRoute(
-          path: '/',
-          pageBuilder: (context, state) => const NoTransitionPage(
-            child: FirstPage(),
-          ),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) => ScaffoldWithNavigation(
+        navigationShell: navigationShell,
+      ),
+      branches: [
+        StatefulShellBranch(
           routes: [
             GoRoute(
-              path: 'detail',
-              builder: (context, state) => const DetailPage(),
+              path: '/',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: FirstPage(),
+              ),
+              routes: [
+                GoRoute(
+                  path: 'detail',
+                  builder: (context, state) => const DetailPage(),
+                ),
+              ],
             ),
           ],
         ),
-        GoRoute(
-          path: '/second',
-          pageBuilder: (context, state) => const NoTransitionPage(
-            child: SecondPage(),
-          ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/second',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: SecondPage(),
+              ),
+            ),
+          ],
         ),
-        GoRoute(
-          path: '/third',
-          pageBuilder: (context, state) => const NoTransitionPage(
-            child: ThirdPage(),
-          ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/third',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: ThirdPage(),
+              ),
+            ),
+          ],
         ),
       ],
     ),
