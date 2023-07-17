7e88ef2a204a79ed3352cbbec39df1b57c885026
htsuruo <tsuru.dev@gmail.com>
2023-07-17 11:04:56 +0900

    Add timer and cancel button to ThirdPage

--- lib/features/third_page.dart
+++ lib/features/third_page.dart
@@ -1,15 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router_playground/navigation/navigation_view.dart';
 
-class ThirdPage extends StatelessWidget {
+class ThirdPage extends StatefulWidget {
   const ThirdPage({super.key});
 
   @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  late Timer timer;
+  var _tick = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // StatefulShellRouteではindexedされているだけなので、画面表示されていなくともタイマーは動き続ける
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _tick = timer.tick;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const NavigationView(
+    final theme = Theme.of(context);
+    return NavigationView(
       titleLabel: 'ShellBranch Root - Third',
       child: Center(
-        child: Text('Third'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Timer: $_tick',
+              style: theme.textTheme.bodyLarge,
+            ),
+            TextButton(
+              onPressed: () {
+                timer.cancel();
+              },
+              child: const Text('Cancel'),
+            )
+          ],
+        ),
       ),
     );
   }
