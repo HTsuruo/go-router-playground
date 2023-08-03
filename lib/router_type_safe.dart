da97886f46b80f53def4e5eaa0dcf2442a171aea
htsuruo <tsuru.dev@gmail.com>
2023-08-03 22:50:48 +0900

    Generate route with type safe

--- lib/router_type_safe.dart
+++ lib/router_type_safe.dart
@@ -1,39 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:go_router_playground/features/first/first_detail_page.dart';
 import 'package:go_router_playground/features/second/second_detail_page.dart';
 import 'package:go_router_playground/features/second/second_page.dart';
 import 'package:go_router_playground/features/third_page.dart';
 
 import 'features/first/first_page.dart';
 
-// part 'router_type_safe.g.dart';
+part 'router_type_safe.g.dart';
 
-// ref. https://github.com/flutter/packages/pull/4238
-// StatefulShellRouteのbuilderは未対応
-// @TypedStatefulShellRoute(
-//   branches: [
-//     firstBranch,
-//     secondBranch,
-//     thirdBranch,
-//   ],
-// )
-
-// **************************************************************************
-// First Page
-// **************************************************************************
-const firstBranch = TypedStatefulShellBranch(
-  routes: [
-    TypedGoRoute<FirstPageRoute>(
-      path: '/first',
+// example: https://github.com/flutter/packages/blob/main/packages/go_router_builder/example/lib/stateful_shell_route_example.dart
+@TypedStatefulShellRoute<ShellRouteData>(
+  branches: [
+    TypedStatefulShellBranch(
       routes: [
-        TypedGoRoute<SecondDetailPageRoute>(
-          path: 'detail',
+        TypedGoRoute<FirstPageRoute>(
+          path: '/first',
+          routes: [
+            TypedGoRoute<FirstDetailPageRoute>(
+              path: 'detail',
+            )
+          ],
         )
       ],
-    )
+    ),
+    TypedStatefulShellBranch(
+      routes: [
+        TypedGoRoute<SecondPageRoute>(
+          path: '/second',
+          routes: [
+            TypedGoRoute<SecondDetailPageRoute>(
+              path: ':index',
+            )
+          ],
+        )
+      ],
+    ),
+    TypedStatefulShellBranch(
+      routes: [
+        TypedGoRoute<ThirdPageRoute>(
+          path: '/third',
+        )
+      ],
+    ),
   ],
-);
+)
+class ShellRouteData extends StatefulShellRouteData {
+  const ShellRouteData();
 
+  @override
+  Widget builder(
+    BuildContext context,
+    GoRouterState state,
+    StatefulNavigationShell navigationShell,
+  ) {
+    return navigationShell;
+  }
+
+  // これを指定すると.indexedStackされなくなる？
+  // ref. https://github.com/flutter/packages/blob/9323e33ed9d8345d87514711dcaeb4cf4159ad1c/packages/go_router/lib/src/route_data.dart#L269-L278
+  // static Widget $navigatorContainerBuilder(
+  //   BuildContext context,
+  //   StatefulNavigationShell navigationShell,
+  // ) {
+  //   return ScaffoldWithNavigation(
+  //     navigationShell: navigationShell,
+  //   );
+  // }
+}
+
+// First Page
 @immutable
 class FirstPageRoute extends GoRouteData {
   const FirstPageRoute();
@@ -50,26 +86,11 @@ class FirstDetailPageRoute extends GoRouteData {
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return const FirstPage(detailPath: '/first/detail');
+    return const FirstDetailPage();
   }
 }
 
-// **************************************************************************
 // Second Page
-// **************************************************************************
-const secondBranch = TypedStatefulShellBranch(
-  routes: [
-    TypedGoRoute<SecondPageRoute>(
-      path: '/second',
-      routes: [
-        TypedGoRoute<SecondDetailPageRoute>(
-          path: ':index',
-        )
-      ],
-    )
-  ],
-);
-
 @immutable
 class SecondPageRoute extends GoRouteData {
   const SecondPageRoute();
@@ -92,17 +113,7 @@ class SecondDetailPageRoute extends GoRouteData {
   }
 }
 
-// **************************************************************************
 // Third Page
-// **************************************************************************
-const thirdBranch = TypedStatefulShellBranch(
-  routes: [
-    TypedGoRoute<ThirdPageRoute>(
-      path: '/third',
-    )
-  ],
-);
-
 @immutable
 class ThirdPageRoute extends GoRouteData {
   const ThirdPageRoute();
---
+++ lib/router_type_safe.g.dart
@@ -0,0 +1,150 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router_type_safe.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $shellRouteData,
+    ];
+
+RouteBase get $shellRouteData => StatefulShellRouteData.$route(
+      factory: $ShellRouteDataExtension._fromState,
+      branches: [
+        StatefulShellBranchData.$branch(
+          routes: [
+            GoRouteData.$route(
+              path: '/first',
+              factory: $FirstPageRouteExtension._fromState,
+              routes: [
+                GoRouteData.$route(
+                  path: 'detail',
+                  factory: $FirstDetailPageRouteExtension._fromState,
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranchData.$branch(
+          routes: [
+            GoRouteData.$route(
+              path: '/second',
+              factory: $SecondPageRouteExtension._fromState,
+              routes: [
+                GoRouteData.$route(
+                  path: ':index',
+                  factory: $SecondDetailPageRouteExtension._fromState,
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranchData.$branch(
+          routes: [
+            GoRouteData.$route(
+              path: '/third',
+              factory: $ThirdPageRouteExtension._fromState,
+            ),
+          ],
+        ),
+      ],
+    );
+
+extension $ShellRouteDataExtension on ShellRouteData {
+  static ShellRouteData _fromState(GoRouterState state) =>
+      const ShellRouteData();
+}
+
+extension $FirstPageRouteExtension on FirstPageRoute {
+  static FirstPageRoute _fromState(GoRouterState state) =>
+      const FirstPageRoute();
+
+  String get location => GoRouteData.$location(
+        '/first',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+extension $FirstDetailPageRouteExtension on FirstDetailPageRoute {
+  static FirstDetailPageRoute _fromState(GoRouterState state) =>
+      const FirstDetailPageRoute();
+
+  String get location => GoRouteData.$location(
+        '/first/detail',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+extension $SecondPageRouteExtension on SecondPageRoute {
+  static SecondPageRoute _fromState(GoRouterState state) =>
+      const SecondPageRoute();
+
+  String get location => GoRouteData.$location(
+        '/second',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+extension $SecondDetailPageRouteExtension on SecondDetailPageRoute {
+  static SecondDetailPageRoute _fromState(GoRouterState state) =>
+      SecondDetailPageRoute(
+        index: int.parse(state.pathParameters['index']!),
+      );
+
+  String get location => GoRouteData.$location(
+        '/second/${Uri.encodeComponent(index.toString())}',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+extension $ThirdPageRouteExtension on ThirdPageRoute {
+  static ThirdPageRoute _fromState(GoRouterState state) =>
+      const ThirdPageRoute();
+
+  String get location => GoRouteData.$location(
+        '/third',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
